1bd5ed86329a9c5c0c997e82f15ed06114e5639d
yutas123 <ikayaki01@gmail.com>
2025-01-06 20:32:15 +0900

    カウントゲーム

--- src/game/scenes/GameScene.js
+++ src/game/scenes/GameScene.js
@@ -1,40 +1,43 @@
-import { DefaultScene } from 'phaser-ez'
+import Phaser from 'phaser'
 
-export default class GameScene extends DefaultScene {
+export default class GameScene extends Phaser.Scene {
   constructor() {
-    super('GameScene')
+    super({ key: 'GameScene' })
+    this.clickCount = 0
   }
 
-  start() {
-    // プレイヤー
-    this.player = this.add.image(512, 700, 'player')
-    this.player.setScale(0.5)
+  create() {
+    // 画面の中央座標を取得
+    const centerX = this.cameras.main.centerX
+    const centerY = this.cameras.main.centerY
 
-    // キー入力の設定
-    this.keyLeft = this.input.keyboard.addKey('LEFT')
-    this.keyRight = this.input.keyboard.addKey('RIGHT')
-    this.keySpace = this.input.keyboard.addKey('SPACE')
-
-    // スコアの初期化
-    this.score = 0
-    this.scoreText = this.add.text(16, 16, 'Score: 0', {
+    // クリック回数表示テキスト
+    this.countText = this.add.text(centerX, centerY - 100, 'クリック回数: 0', {
       fontSize: '32px',
       fill: '#fff'
+    }).setOrigin(0.5)
+
+    // クリックボタン作成
+    this.button = this.add.rectangle(centerX, centerY, 200, 100, 0x00ff00)
+    this.button.setInteractive()
+    
+    // ボタンテキスト
+    this.buttonText = this.add.text(centerX, centerY, 'クリック！', {
+      fontSize: '32px',
+      fill: '#000'
+    }).setOrigin(0.5)
+
+    // ボタンクリックイベント
+    this.button.on('pointerdown', () => {
+      this.clickCount++
+      this.countText.setText(`クリック回数: ${this.clickCount}`)
+
+      if (this.clickCount >= 10) {
+        this.countText.setText('クリア！おめでとう！')
+        this.button.setFillStyle(0xff0000)
+        this.buttonText.setText('完了')
+        this.button.disableInteractive()
+      }
     })
-  }
-
-  update() {
-    // プレイヤーの移動
-    if (this.keyLeft && this.keyLeft.isDown && this.player.x > 50) {
-      this.player.x -= 5
-    }
-    if (this.keyRight && this.keyRight.isDown && this.player.x < 974) {
-      this.player.x += 5
-    }
-  }
-
-  dispose() {
-    if (this.player) this.player.destroy()
-    if (this.scoreText) this.scoreText.destroy()
   }
 }
